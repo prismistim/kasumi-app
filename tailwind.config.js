dd38693bd3b76bcb0c611b0cae176d0ffb753c1c
snowsphere <info@snowsphere.net>
2023-12-30 01:07:24 +0900

    change: スタイル更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,8 @@ export default {
       {
         mytheme: {
           ...require("daisyui/src/theming/themes")["black"],
-          "primary": "#3de2bc",
-          "secondary": "#8ad1b2",
+          "primary": "#9ac427",
+          "secondary": "#71def7",
           "accent": "#e23d61",
           "neutral": "#3d4451",
           "base-100": "#151515"
